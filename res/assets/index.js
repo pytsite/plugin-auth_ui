eda83d7928fe5f85e0f8a54ef807cbba0a3cf229
Oleksandr Shepetko <a@shepetko.com>
2018-10-23 19:13:12 +0300

    Bugfix 4.2.1

--- res/assets/index.js
+++ res/assets/index.js
@@ -3,4 +3,6 @@ import UserSearchModal from './components/UserSearchModal'
 import UserSelectSearch from './components/UserSelectSearch'
 import UsersSlots from './components/UsersSlots'
 
+import './index.scss';
+
 export {UserPicture, UserSearchModal, UserSelectSearch, UsersSlots}
